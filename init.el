ae66e5cba9a5ac68f83ee278e6e685a57eadaf23
strangeloop <strangeloop@tower.local>
2023-10-06 18:53:15 -0400

    Added web and shader mode support

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(markdown-mode which-key visual-fill-column use-package sudo-edit rg rainbow-mode rainbow-delimiters neotree lua-mode leuven-theme ivy-rich helpful gnu-elpa-keyring-update evil-collection eglot diminish counsel company clojure-ts-mode cider)))
+   '(shader-mode web-mode markdown-mode which-key visual-fill-column use-package sudo-edit rg rainbow-mode rainbow-delimiters neotree lua-mode leuven-theme ivy-rich helpful gnu-elpa-keyring-update evil-collection eglot diminish counsel company clojure-ts-mode cider)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
