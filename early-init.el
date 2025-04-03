1d4bd2b9bf2afaa591edc3a938b6ab5f224eb18c
strnglp <contact@strangeloop.blog>
2025-04-03 06:55:09 -0400

    Better font/face handling and more robust frame parameters

--- early-init.el
+++ early-init.el
@@ -24,12 +24,8 @@
   (push (apply #'format format-string args) startup-message-log))
 (advice-add 'message :override #'silent-message)
 
-(tool-bar-mode -1)
 (tooltip-mode -1)
-(menu-bar-mode -1)
 (blink-cursor-mode 0)
-(scroll-bar-mode 0)
-(fringe-mode 0)
 (set-display-table-slot standard-display-table 'truncation
                         (make-glyph-code ?… 'default))
 (set-display-table-slot standard-display-table 'wrap
@@ -42,14 +38,23 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 🎨 APPEARANCE CUSTOMIZATION ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar base-frame-parameters
+  '((width . 120) (height . 48)
+    (vertical-scroll-bars . nil)
+    (tool-bar-lines . 0)
+    (menu-bar-lines . 0)
+    (left-fringe . 0)
+    (right-fringe . 0))
+  "Base parameters for initial and default Emacs frames.")
+
 (setq
  inhibit-compacting-font-caches t
  my/mono-font "JetBrainsMono NF-16"
  my/mono-face "JetBrainsMono NF 16"
  my/variable-face "Linux Libertine 20"
  my/org-header-face "Linux Libertine Capitals 20"
- initial-frame-alist '((width . 120) (height . 48))
- default-frame-alist '((width . 120) (height . 48))
+ initial-frame-alist base-frame-parameters
+ default-frame-alist base-frame-parameters
  frame-resize-pixelwise t
  frame-inhibit-implied-resize t
  frame-title-format '("Emacs"))
@@ -58,16 +63,10 @@
  ((eq system-type 'gnu/linux)
   ())
  ((eq system-type 'darwin)
-  (setq-local
+  (setq
    my/mono-font "JetBrainsMono Nerd Font-16"
    my/mono-face "JetBrainsMono Nerd Font 16"))
  (t ()))
-
-(defvar after-load-theme-hook nil
-  "Hook run after a color theme is loaded using `load-theme'.")
-(defadvice load-theme (after run-after-load-theme-hook activate)
-  "Run `after-load-theme-hook'."
-  (run-hooks 'after-load-theme-hook))
 
 (defun set-faces ()
   (set-face-attribute 'default nil :font my/mono-face)
@@ -77,19 +76,28 @@
   (set-face-foreground 'vertical-border (face-background 'mode-line-inactive)))
 
 (defun set-org-faces ()
-  (with-eval-after-load 'org
+  (when (featurep 'org)
     (when (facep 'org-meta-line)
       (set-face-attribute 'org-meta-line nil :inherit 'fixed-pitch))
     (set-face-attribute 'org-block nil :inherit 'fixed-pitch)
     (set-face-attribute 'org-block-begin-line nil :inherit 'fixed-pitch)
     (set-face-attribute 'org-block-end-line nil :inherit 'org-block-begin-line)
     (set-face-attribute 'org-document-title nil :font my/org-header-face :height 1.5)
-    (set-face-attribute 'org-level-1 nil :font my/org-header-face :height 1.1)
-    (set-face-attribute 'org-level-2 nil :font my/org-header-face :height 1.1)
-    (set-face-attribute 'org-level-3 nil :font my/org-header-face :height 1.1)
-    (set-face-attribute 'org-level-4 nil :font my/org-header-face :height 1.1))
-  (with-eval-after-load 'org-superstar-mode
-    (set-face-attribute 'org-superstar-item nil :inherit 'fixed-pitch :height 120)))
+    (set-face-attribute 'org-level-1 nil :font my/org-header-face)
+    (set-face-attribute 'org-level-2 nil :font my/org-header-face)
+    (set-face-attribute 'org-level-3 nil :font my/org-header-face)
+    (set-face-attribute 'org-level-4 nil :font my/org-header-face))
+  (when (featurep 'org-superstar)
+    (set-face-attribute 'org-superstar-item nil :inherit 'fixed-pitch)))
+
+(with-eval-after-load 'org
+  (set-org-faces))
+
+
+(defun run-after-load-theme-hook (&rest _)
+  "Run `after-load-theme-hook'."
+  (run-hooks 'after-load-theme-hook))
+(advice-add 'load-theme :after #'run-after-load-theme-hook)
 
 (add-hook 'org-mode #'set-org-faces)
 (add-hook 'org-superstar-mode #'set-org-faces)
